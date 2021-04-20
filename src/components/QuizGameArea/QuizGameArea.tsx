c3749d0a8739e6dab3a92e7d26730d4d30604f82
Nelson <nelson.ortega@microfocus.com>
2021-04-20 16:50:19 -0600

    New ShowComponent component

--- src/components/QuizGameArea/QuizGameArea.tsx
+++ src/components/QuizGameArea/QuizGameArea.tsx
@@ -2,20 +2,27 @@ import './QuizGameArea.css'
 import { useState } from 'react'
 import QuizQuestion from '../QuizQuestion/QuizQuestion'
 import { questionsArray } from '../../data/Questions'
+import ShowComponent from '../ShowComponent/ShowComponent'
 
 const QuizGameArea = () => {
   const [score, setScore] = useState<number>(0)
   const [quantityOfQuestionsAswered, setQuantityOfQuestionsAswered] = useState<number>(0)
+
+  const isGameFinished = quantityOfQuestionsAswered === questionsArray.length
   
   return (
     <div className="game-area-container">
-      {quantityOfQuestionsAswered !== questionsArray.length ?
-      <QuizQuestion 
-        setScore={setScore}
-        questions={questionsArray}
-        quantityOfQuestionsAswered={quantityOfQuestionsAswered}
-        setQuantityOfQuestionsAswered={setQuantityOfQuestionsAswered}
-      /> : score }
+      <ShowComponent show={!isGameFinished}>
+        <QuizQuestion 
+          setScore={setScore}
+          questions={questionsArray}
+          quantityOfQuestionsAswered={quantityOfQuestionsAswered}
+          setQuantityOfQuestionsAswered={setQuantityOfQuestionsAswered}
+        />
+      </ShowComponent>
+      <ShowComponent show={isGameFinished}>
+        {score}
+      </ShowComponent>
     </div>
   )
 }
