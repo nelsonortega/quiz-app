968ec2256998e639bb8a8deb838c4187964ed027
Nelson <neor9712@gmail.com>
2021-05-09 18:57:43 -0600

    Questions updated

--- src/data/Questions.ts
+++ src/data/Questions.ts
@@ -2,43 +2,43 @@ import { Answers, Question } from "../interfaces/IQuestion"
 
 export const questionsArray: Array<Question> = [
   {
-    question: 'as',
-    optionA: 'as',
-    optionB: 'as',
-    optionC: 'as',
-    optionD: 'as',
+    question: 'What is the capital of France?',
+    optionA: 'Lyon',
+    optionB: 'Marsella',
+    optionC: 'Paris',
+    optionD: 'Toulouse',
+    correctAnswer: Answers.C
+  },
+  {
+    question: 'What is the color of the letter G in the Google logo?',
+    optionA: 'Yellow',
+    optionB: 'Blue',
+    optionC: 'Red',
+    optionD: 'Green',
+    correctAnswer: Answers.B
+  },
+  {
+    question: 'Which singer was known amongst other things as "The King of Pop"?',
+    optionA: 'Michael Jackson',
+    optionB: 'Lady Gaga',
+    optionC: 'Justin Bieber',
+    optionD: 'Demi Lovato',
     correctAnswer: Answers.A
   },
   {
-    question: 'sdf',
-    optionA: 'ads',
-    optionB: 'afds',
-    optionC: 'adfs',
-    optionD: 'aggggs',
-    correctAnswer: Answers.A
+    question: 'What was the most downloaded app of 2020?',
+    optionA: 'Youtube',
+    optionB: 'Tik Tok',
+    optionC: 'Facebook',
+    optionD: 'Tinder',
+    correctAnswer: Answers.B
   },
   {
-    question: 'assss',
-    optionA: 'asdas',
-    optionB: 'ggfdas',
-    optionC: 'fhas',
-    optionD: 'afghs',
-    correctAnswer: Answers.A
-  },
-  {
-    question: 'as',
-    optionA: 'astyu',
-    optionB: 'ahfgts',
-    optionC: 'as',
-    optionD: 'as',
-    correctAnswer: Answers.A
-  },
-  {
-    question: 'affs',
-    optionA: 'asfs',
-    optionB: 'asss',
-    optionC: 'adsfs',
-    optionD: 'as',
+    question: 'Which year was the Premier League founded?',
+    optionA: '1992',
+    optionB: '2001',
+    optionC: '1985',
+    optionD: '2010',
     correctAnswer: Answers.A
   },
 ]
